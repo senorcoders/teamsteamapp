7d2b5779bd56621bfa55d5c0f0e557b89dc38a9c
Osmany Jirón <osmany@senorcoders.com>
2018-05-29 10:32:47 -0600

    agregue el button back para camera

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -85,9 +85,9 @@ export class CameraPage {
 
     let previewOtions: CameraPreviewOptions = {
       x: 0,
-      y: 0,
-      width: window.innerWidth,
-      height: window.innerHeight - 80,
+      y: 56,
+      width: window.screen.width,
+      height: window.screen.height - 156,
       camera: 'rear',
       tapPhoto: false,
       previewDrag: false,
