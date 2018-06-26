38cdab3b7c8a7524335882b46bf87fa3877693f2
Osmany Jirón <osmany@senorcoders.com>
2018-06-26 11:11:30 -0600

    finalizada la opcion de asistencia en eventos

--- src/components/assistence/assistence.ts
+++ src/components/assistence/assistence.ts
@@ -11,7 +11,7 @@ import { MyApp } from '../../app/app.component';
 export class AssistenceComponent {
 
   public players: Array<any> = [];
-  public assistence: any = {};
+  public assistence: any = { players: [] };
   public event: any = {};
   public search = "";
   public now = new Date();
@@ -20,36 +20,44 @@ export class AssistenceComponent {
     public http: HttpClient
   ) {
     this.event = this.navParams.get("event");
+    
     if (this.navParams.get("assistence") !== undefined) {
       this.assistence = this.navParams.get("assistence");
+      console.log(this.assistence);
     }
 
   }
 
   async ionViewDidLoad() {
     try {
-      this.players = await this.http.get('/players?where={"team":"' + MyApp.User.team + '"}').toPromise() as any;
-
-      this.players = this.players.map(function (it) {
-
-        if (this.assistence.hasOwnProperty("id")) {
-          let assistence = this.assistence.players.find(function (id) { return id.id === it.id; });
-          if (assistence !== undefined) {
-            it.status = assistence.status;
-            it.late = assistence.late;
-          }
-        }else{
-          it.status = "";
-          it.late = "";
-        }
-
-        return it;
-      }.bind(this));
-
+      if (!this.assistence.hasOwnProperty("id")) {
+        this.players = await this.http.get('/players?where={"team":"' + MyApp.User.team + '"}').toPromise() as any;
+        this.factorize();
+      }else{
+        this.players = this.assistence.players;
+      }
     }
     catch (e) {
       console.error(e);
     }
+  }
+
+  private factorize() {
+    this.players = this.players.map(function (it) {
+
+      if (this.assistence.hasOwnProperty("id")) {
+        let assistence = this.assistence.players.find(function (id) { return id.id === it.id; });
+        if (assistence !== undefined) {
+          it.status = assistence.status;
+          it.late = assistence.late;
+        }
+      } else {
+        it.status = "";
+        it.late = false;
+      }
+
+      return it;
+    }.bind(this));
   }
 
   public async byPass() { }
@@ -68,16 +76,83 @@ export class AssistenceComponent {
 
   }
 
-  public async asingStatus(status) {
+  public async asingStatus(player, status) {
     try {
 
-      if (!this.assistence.hasOwnProperty("id")) {
-        this.assistence = await this.http.post("/assistenceevents", {
-          dateTime: new Date().toISOString(),
-          players: [],
-          event: this.event.id
+      if (player.status === status)
+        player.status = "";
+      else
+        player.status = status;
+
+      if (this.assistence.players.length === 0) {
+        this.assistence.players.push({
+          id: player.id,
+          status: player.status,
+          late: player.late
         });
+      } else {
+        let index = this.assistence.players.findIndex(function (it) { return it.id === player.id; });
+        if (index === -1) {
+          this.assistence.players.push({
+            id: player.id,
+            status: player.status,
+            late: player.late
+          });
+        } else {
+          this.assistence.players[index].status = player.status;
+          this.assistence.players[index].late = player.late;
+        }
+
       }
+
+      if (!this.assistence.hasOwnProperty("id")) {
+        this.assistence.dateTime = new Date().toISOString();
+        this.assistence.event = this.event.id
+        this.assistence = await this.http.post("/assistenceevents", this.assistence).toPromise();
+      }else{
+        await this.http.put("/assistenceevents/"+ this.assistence.id, this.assistence).toPromise();
+      }
+      
+    }
+    catch (e) {
+      console.error(e);
+    }
+
+  }
+
+  public async asingLate(player) {
+    try {
+
+      player.late = !player.late;
+
+      if (this.assistence.players.length === 0) {
+        this.assistence.players.push({
+          id: player.id,
+          status: player.status,
+          late: player.late
+        });
+      } else {
+        let index = this.assistence.players.findIndex(function (it) { return it.id === player.id; });
+        if (index === -1) {
+          this.assistence.players.push({
+            id: player.id,
+            status: player.status,
+            late: player.late
+          });
+        } else {
+          this.assistence.players[index].late = player.late;
+        }
+
+      }
+
+      if (!this.assistence.hasOwnProperty("id")) {
+        this.assistence.dateTime = new Date().toISOString();
+        this.assistence.event = this.event.id
+        this.assistence = await this.http.post("/assistenceevents", this.assistence).toPromise();
+      }else{
+        await this.http.put("/assistenceevents/"+ this.assistence.id, this.assistence).toPromise();
+      }
+      
     }
     catch (e) {
       console.error(e);
--- src/components/assistences/assistences.ts
+++ src/components/assistences/assistences.ts
@@ -4,6 +4,8 @@ import { ModalController } from 'ionic-angular';
 import { AssistenceComponent } from '../assistence/assistence';
 import { NavParams } from 'ionic-angular';
 import { MyApp } from '../../app/app.component';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 
 @Component({
@@ -14,11 +16,25 @@ export class AssistencesComponent {
 
   public event:any;
   public user=MyApp.User;
+  public assistences=[];
 
   constructor(public viewCtrl: ViewController, public modalCtrl: ModalController,
-    public navParams: NavParams
+    public navParams: NavParams, public http: HttpClient
   ) {
     this.event = this.navParams.get("event");
+  }
+
+  async ionViewWillEnter(){
+    this.assistences = await this.http.get("/assistence/"+ this.event.id).toPromise() as any;
+    this.assistences = this.assistences.map(function(it){
+      it.dateTime = moment(it.dateTime).toDate();
+      return it;
+    });
+  }
+
+  public toAssitence(assistence){
+    this.modalCtrl.create(AssistenceComponent, {event: this.event, assistence})
+    .present();
   }
 
   public addSheetAtendence(){
