719cd1ee66833dc8bfb0d3b6ae177cf98a7d671c
Osmany Jirón <osmany@senorcoders.com>
2018-09-18 14:54:01 -0600

    agregue la funcion para eliminar cuenta pero aun no listo, y los usuarios tiene la opcion de no tener rol en el cual no se les mostraria ningun item en el menu, solo para entrar en profile

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { MyApp } from '../../app/app.component';
 import { HelpersProvider } from '../../providers/helpers/helpers';
+import { Storage } from '@ionic/storage';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 
 class Setting {
@@ -31,7 +33,7 @@ export class SettingPage {
   public version = "0.0.8"
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public http: HttpClient
+    public http: HttpClient, private storage: Storage, private auth: AuthServiceProvider
   ) {
     this.setting = new Setting();
     this.userRole = MyApp.User.role.name;
@@ -42,7 +44,7 @@ export class SettingPage {
     let load = HelpersProvider.me.getLoadingStandar();
 
     try {
-      
+
       //Para cuando el usuario es manager
       if (this.userRole === "Manager") {
         let team = await this.http.get("/teams/" + MyApp.User.team).toPromise() as any;
@@ -77,4 +79,90 @@ export class SettingPage {
     }
   }
 
+  public async removeAccount() {
+    let load = HelpersProvider.me.getLoadingStandar();
+    try {
+      if (MyApp.User.role.name === "Manager") {
+
+        await this.http.delete("/roles/" + MyApp.User.role.id).toPromise();
+      } else if (MyApp.User.role.name === "Player") {
+
+        let player: any[];
+        if (Object.prototype.toString.call(MyApp.User.role.team) === "[object String]") {
+          player = await this.http.get(`/players?where={"user":"${MyApp.User.id}","team":"${MyApp.User.role.team}"}`).toPromise() as any[];
+        } else {
+          player = await this.http.get(`/players?where={"user":"${MyApp.User.id}","team":"${MyApp.User.role.team.id}"}`).toPromise() as any[];
+        }
+        if (player.length === 1) {
+          await this.http.delete("/roles/" + MyApp.User.role.id).toPromise();
+          await this.http.delete("/players/" + player[0].id).toPromise();
+        }
+
+      } else if (MyApp.User.role.name === "Family") {
+
+        let familys: any[];
+        if (Object.prototype.toString.call(MyApp.User.role.team) === "[object String]") {
+          familys = await this.http.get(`/family?where={"user":"${MyApp.User.id}","team":"${MyApp.User.role.team}"}`).toPromise() as any[];
+        } else {
+          familys = await this.http.get(`/family?where={"user":"${MyApp.User.id}","team":"${MyApp.User.role.team.id}"}`).toPromise() as any[];
+        }
+        for (let family of familys) {
+          await this.http.delete("/family/" + family.id).toPromise();
+        }
+        await this.http.delete("/roles/" + MyApp.User.role.id).toPromise();
+      }
+
+      //Actualizar el user en la base de datos local
+      let index = MyApp.User.roles.findIndex(it => {
+        return it.id === MyApp.User.role.id;
+      });
+      if (index !== -1) {
+        if (MyApp.User.roles.length === 1) {
+          MyApp.User.roles = [];
+        } else {
+          MyApp.User.roles.splice(index, 1);
+        }
+        await this.storage.set("user", MyApp.User);
+      }
+
+      //Delete role en la base de datos local
+      delete MyApp.User.role;
+      await this.storage.remove("role");
+
+      await this.changeRol();
+    }
+    catch (e) {
+      console.error(e);
+      await HelpersProvider.me.presentAlertErrorStandar();
+    }
+
+    load.dismiss();
+  }
+
+  private async changeRol() {
+    let role: any;
+    if (MyApp.User.roles.length === 0) return;
+
+    role = MyApp.User.roles[0];
+
+    if (role.name !== "FreeAgent" && role.name !== "OwnerLeague") {
+      try {
+        await this.auth.setTimeZoneTeam();
+        await HelpersProvider.me.setGeofences(200);
+      }
+      catch (e) {
+        console.error(e);
+      }
+    } else {
+
+      try {
+        await HelpersProvider.me.stopGeofences();
+      }
+      catch (e) {
+        console.error(e);
+      }
+    }
+
+    await this.auth.updateRole(role);
+  }
 }
