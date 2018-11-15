8e0fb86cb99b8460278224515689e78e715274e7
Osmany Jirón <osmany@senorcoders.com>
2018-11-15 16:22:28 -0600

    agregue la pantalla de tabs

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EventsSchedulePage } from '../events-schedule/events-schedule';
+import { RosterPage } from '../roster/roster';
+import { ListChatsPage } from '../list-chats/list-chats';
+import { PhotosPage } from '../photos/photos';
+import { ViewProfilePage } from '../view-profile/view-profile';
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+
+  events = EventsSchedulePage;
+  roster = RosterPage;
+  chat = ListChatsPage;
+  photos = PhotosPage;
+  more = ViewProfilePage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+  }
+
+}
